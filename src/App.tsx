77f22a4cb555ec20b606c8028b5fc06a9f0cc555
Virginija Jan <vjanuskaite@gmail.com>
2020-06-18 15:28:12 +0300

    Enable routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import TestTable from "./components/myTable/table";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <TestTable />;
+  return (
+    <Router>
+      <TestTable />
+    </Router>
+  );
 }
 
 export default App;
